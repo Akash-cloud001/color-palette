6873453a6b025535a59b527b036e2a3c64b09859
Akash Parmar <ak.parmar1442000@gmail.com>
2023-04-03 22:04:01 +0530

    added CopyClipboard animation so user get to know that it's copied

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import '../styles/BoxColor.css';
 
 const BoxColor = ({name,color}) => {
+  const [copied, setCopied] = useState(false);
+  const changeCopy = ()=>{
+    setCopied(true);
+    setTimeout(()=> setCopied(false),2000);
+  }
+  
   return (
-    <CopyToClipboard text={color}>
+    <CopyToClipboard text={color} onCopy={changeCopy}>
       <div className='BoxColor' style={{backgroundColor:color}}>
+        <div className={`copy-overlay ${copied && 'show'}`} style={{background:color}}/>
+        <div className={`copy-msg ${copied && 'show'}`}>
+          <h1>
+            copied
+          </h1>
+          <p>
+            {color}
+          </p>
+        </div>
         <div className='copy-content'>{name}</div>
         <button className='copy-button'>Copy</button>
         <button className='see-more'>More</button>
