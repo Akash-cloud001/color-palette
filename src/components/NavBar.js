187dc6a8953a07ad626b6fb856820366c1ed75c2
Akash Parmar <ak.parmar1442000@gmail.com>
2023-04-04 15:33:11 +0530

    added Select using MaterialUi and implemented format Changer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Select, MenuItem } from '@mui/material';
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import logo from '../images/logo.png';
 import '../styles/NavBar.css';
 
 
-const NavBar = ({level, changeLevel}) => {
+const NavBar = ({level, changeLevel, changeSelect}) => {
+    const [format, setFormat] = useState('hex');
+    const handleChangeSelect = (e)=>{
+        setFormat(e.target.value);
+        changeSelect(e.target.value);
+    }
   return (
     <header className='navbar'>
         <div className='logo'>
@@ -20,6 +26,17 @@ const NavBar = ({level, changeLevel}) => {
             step={100} 
             onChange={changeLevel}
             />
+        </div>
+        <div className='select-container'>
+            <Select 
+            value={format} 
+            onChange={handleChangeSelect}
+            variant='standard' 
+            > 
+                <MenuItem value='hex'>HEX-#ffffff</MenuItem>
+                <MenuItem value='rgb'>RGB-(255,255,255)</MenuItem>
+                <MenuItem value='rgba'>RGBA-(255,255,255,1.0)</MenuItem>
+            </Select>
         </div>
     </header>
   )
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -7,16 +7,24 @@ import BoxColor from './BoxColor';
 
 const Palette = (props) => {
   const [level,setLevel] = useState(500);
+  const [format, setFormat] = useState('hex');
   const changeLevel = (newLevel) =>{
     setLevel(newLevel);
   }
+  const changeSelect = (val)=>{
+    setFormat(val); 
+  }
   return (
     <div className='palette'>
-      <NavBar level={level} changeLevel={changeLevel} />
+      <NavBar level={level} changeLevel={changeLevel} changeSelect={changeSelect}/>
       <div className='palette-colors'>
-          {props.palette.colors[level].map(color=>
-          <BoxColor color={color.hex} name={color.name
-          }/>)}
+          {props.palette.colors[level]
+          .map(color=>
+            <BoxColor 
+              color={color[format]} 
+              name={color.name}
+            />
+          )}
       </div>
     </div>
   )
