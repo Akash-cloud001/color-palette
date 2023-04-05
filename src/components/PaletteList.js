776207213ed6b918162b98b447a2450ac3ed9d6e
Akash Parmar <ak.parmar1442000@gmail.com>
2023-04-06 00:05:57 +0530

    started styling PaletteList with withStyles

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,22 +1,57 @@
 import React from 'react'
+import { withStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 
+const styles = {
+    root:{
+        backgroundColor : 'blue',
+        height: '100%',
+        display : 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container:{
+        width : '50%',
+        display : 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap:'wrap',
+        border:'1px solid black'
+    },
+    nav:{
+        display:'flex',
+        width : '100%',
+        justifyContent: 'space-between',
+        color:'white'
+    },
+    palettes:{
+        boxSizing:'border-box',
+        width : '100%',
+        display : 'grid',
+        gridTemplateColumns: 'repeat(3, 30%)',
+        gridGap: '5%'
+    }
+}
+
+
 const PaletteList = (props) => {
-    const {palette} = props;
+    const {classes,palette} = props;
     console.log(palette)
     return (
-    <div>
-        <h2>Color Palette</h2>
-        {palette.map((pal)=>(
-            // <div>
-            //     <Link to={`/palette/${pal.id}`}>{pal.paletteName}</Link>
-            // </div>
-            // name = {pal.paletteName} id = {pal.id} emoji = {pal.emoji} colors = {pal.colors}
-            <MiniPalette {...pal} />
-        ))}
+    <div className={classes.root}>
+        <div className={classes.container}>
+            <nav className={classes.nav}>
+                <h2>Color Palette</h2>
+            </nav>
+            <div className={classes.palettes}>
+                {palette.map((pal)=>(
+                    <MiniPalette {...pal} />
+                    ))}
+            </div>
+        </div>
     </div>
   )
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
