d4dd452567a9db7695fe28433430d71d53bd2f17
Akash Parmar <ak.parmar1442000@gmail.com>
2023-04-11 21:14:27 +0530

    DragColorBox styled

--- src/components/DragColorBox.js
+++ src/components/DragColorBox.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { withStyles } from '@material-ui/styles';
+import chroma from 'chroma-js';
+import DeleteIcon from '@mui/icons-material/Delete';
+function checkLumicity({color}){
+    console.log('in')
+     return chroma(color).luminance() <= 0.4 ? 'white':'black';
+}
 
 const styles = {
     root:{
@@ -9,7 +15,27 @@ const styles = {
         display: 'inline-block',
         position: 'relative',
         cursor : 'pointer',
-        
+        "&:hover svg":{
+            color:'white',
+            scale: '1.3'
+        }
+    },
+    boxContent:{
+        color: 'rgba(0,0,0,0.5)',
+        position:'absolute',
+        width: '100%',
+        left: '0',
+        bottom: '0',
+        padding: '6px 10px',
+        letterSpacing: '1px',
+        textTransform:'uppercase',
+        fontSize: '12px',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    boxName:{
+        fontWeight:'bold'
     }
 }
 
@@ -18,7 +44,12 @@ function DragColorBox(props){
 
     return (
         <div className={classes.root} style={{backgroundColor: color}}> 
-            {name}
+            <div className={classes.boxContent}>
+                <span className={classes.boxName}>
+                    {name}
+                </span>
+                <DeleteIcon style={{transition: 'all 0.3s ease-in-out'}}/>
+            </div>
         </div>
     )
 }
